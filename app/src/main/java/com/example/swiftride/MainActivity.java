e7375fa057cbc4a5ba9d3c180dc9983005f9653b
Nesara Rahal <rahalnesara@gmail.com>
2024-11-03 00:49:54 +0530

    Front end structure for login and register pages

---
+++ app/src/main/java/com/example/swiftride/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.swiftride;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void registerPage(View view){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/swiftride/RegisterActivity.java
@@ -0,0 +1,12 @@
+package com.example.swiftride;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegisterActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_page);
+    }
+}
