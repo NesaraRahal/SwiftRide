2242f26f03c8c6ad1011cadc2db990714d9dd057
Nesara Rahal <rahalnesara@gmail.com>
2024-11-07 10:48:32 +0530

    dashboard

--- app/src/main/java/com/example/swiftride/DashboardActivity.java
+++ app/src/main/java/com/example/swiftride/DashboardActivity.java
@@ -1,6 +1,21 @@
 package com.example.swiftride;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
 public class DashboardActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard_page);
+
+    }
+
+    public void regBus(View view){
+        Intent intent = new Intent(this, RegisterBusActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/swiftride/RegisterBusActivity.java
@@ -0,0 +1,8 @@
+package com.example.swiftride;
+
+import android.app.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterBusActivity extends AppCompatActivity {
+}
